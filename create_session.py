1bccd7268e9b64f35e9a9626841d2ae9f821ce9f
= <akromjonrustamov56@gmail.com>
2026-01-02 16:24:24 +0500

    full commit

--- create_session.py
+++ create_session.py
@@ -1,13 +1,44 @@
+# import instaloader
+# import os
+# from dotenv import load_dotenv
+# from app.config import settings
+# load_dotenv()
+
+# settings.SESSION_DIR.mkdir(parents=True, exist_ok=True)
+
+# L = instaloader.Instaloader()
+# L.login(os.getenv("IG_USERNAME"), os.getenv("IG_PASSWORD"))
+# L.save_session_to_file(filename=str(settings.SESSION_FILE))
+# print("✅ Session saved:", settings.SESSION_FILE)
+
 import instaloader
 import os
 from dotenv import load_dotenv
 from app.config import settings
+
 load_dotenv()
 
 settings.SESSION_DIR.mkdir(parents=True, exist_ok=True)
 
-L = instaloader.Instaloader()
-L.login(os.getenv("IG_USERNAME"), os.getenv("IG_PASSWORD"))
-L.save_session_to_file(filename=str(settings.SESSION_FILE))
-print("✅ Session saved:", settings.SESSION_FILE)
+proxy_url = (
+    f"http://brd-customer-hl_0793675b-zone-scraping_browser1:"
+    f"pxyyd378gevm@"
+    f"brd.superproxy.io:"
+    f"9222"
+)
 
+L = instaloader.Instaloader()
+
+# 🔥 КЛЮЧЕВОЕ
+L.context._session.proxies = {
+    "http": proxy_url,
+    "https": proxy_url,
+}
+
+L.login(
+    os.getenv("IG_USERNAME"),
+    os.getenv("IG_PASSWORD")
+)
+
+L.save_session_to_file(filename=str(settings.SESSION_FILE))
+print("✅ Session saved with proxy:", settings.SESSION_FILE)
