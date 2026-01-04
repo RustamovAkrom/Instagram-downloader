469a2545895de3bb9d1abd44de9fe0c999f76446
= <akromjonrustamov56@gmail.com>
2026-01-05 01:19:03 +0500

    delete session and update create session file

--- create_session.py
+++ create_session.py
@@ -1,12 +1,12 @@
-# import instaloader
-# import os
-# from dotenv import load_dotenv
-# from app.config import settings
-# load_dotenv()
+import instaloader
+import os
+from dotenv import load_dotenv
+from app.config import settings
+load_dotenv()
 
-# settings.SESSION_DIR.mkdir(parents=True, exist_ok=True)
+settings.SESSION_DIR.mkdir(parents=True, exist_ok=True)
 
-# L = instaloader.Instaloader()
-# L.login(os.getenv("IG_USERNAME"), os.getenv("IG_PASSWORD"))
-# L.save_session_to_file(filename=str(settings.SESSION_FILE))
-# print("✅ Session saved:", settings.SESSION_FILE)
+L = instaloader.Instaloader()
+L.login(os.getenv("IG_USERNAME"), os.getenv("IG_PASSWORD"))
+L.save_session_to_file(filename=str(settings.SESSION_FILE))
+print("✅ Session saved:", settings.SESSION_FILE)
